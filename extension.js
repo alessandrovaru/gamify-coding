e854f22e655a8a6f1689bc2d8903fb210fdfa906
Alessandro <alessandrovaru@gmail.com>
2024-01-21 21:43:03 -0400

    Add gamification features to extension.js

--- extension.js
+++ extension.js
@@ -17,6 +17,56 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
+
+	let motivationalPhrases = [
+			'¡Sigue así, lo estás haciendo genial!',
+			'¡No te rindas, el éxito está cerca!',
+			'¡Cada línea de código te acerca a tu objetivo!',
+			'¡La práctica hace al maestro!',
+			'¡Tu esfuerzo vale la pena!',
+			'¡Pronto un lolsito en Discord!',
+			'Cada día mejoras más y más',
+	];
+
+	let timeCounter = 0;
+	let points = 0;
+	let level = 1;
+
+	let intervalId = setInterval(() => {
+		timeCounter++;
+		if (timeCounter === 2) {
+				points += 50;
+				let nextLevelPoints = level * 100;
+				if (points >= nextLevelPoints) {
+						level++;
+						vscode.window.showInformationMessage(`Level Up! You're now level ${level}. Total XP: ${points}`, 'OK');
+						points = 0;
+				} else {
+						let remainingPoints = nextLevelPoints - points;
+						vscode.window.showInformationMessage(`+50 XP! Progress: ${points}/${nextLevelPoints} to next level.`, 'OK');
+				}
+				timeCounter = 0;
+		}
+	}, 1000);
+
+	let watcher = vscode.workspace.createFileSystemWatcher('**', false, true, false);
+
+	watcher.onDidCreate(uri => {
+			points += 25;
+			let nextLevelPoints = level * 100;
+			if (points >= nextLevelPoints) {
+					level++;
+					vscode.window.showInformationMessage(`Level Up! You're now level ${level}. Total XP: ${points}`,  'OK');
+					points = 0;					
+			} else {
+					let remainingPoints = nextLevelPoints - points;
+					vscode.window.showInformationMessage(`+25 XP for creating a file or folder. Progress: ${points}/${nextLevelPoints} to next level.`, 'OK');
+			}
+	});
+
+	context.subscriptions.push(watcher);
+
+
 	let disposable = vscode.commands.registerCommand('gamify-coding.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
